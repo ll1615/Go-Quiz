2b7c770e734919ad6ea7c8431458288c7d2451ef
ll1615 <43992581@qq.com>
2017-01-09 10:20:42 +0800

    Update trace.go

--- lib/trace.go
+++ lib/trace.go
@@ -5,8 +5,9 @@ import (
 	"time"
 )
 
-// Trace a function entering moment and elapsed time
-// It should be called like this: defer lib.Trace()()
+// Trace a function entering moment and elapsed time.
+// It should be called in the very beginning in a function,
+// like this: defer lib.Trace()()
 func Trace() func() {
 	start := time.Now()
 	fmt.Println(start)
