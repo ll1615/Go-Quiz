cd086675537aa1c2896061eaeec761834c4e3e9b
ll1615 <43992581@qq.com>
2017-01-09 17:38:30 +0800

    Update diskUsage.go

--- diskUsage.go
+++ diskUsage.go
@@ -13,6 +13,7 @@ import (
 
 var verbose = flag.Bool("v", false, "show verbose progress messages")
 var sema = make(chan struct{}, 20)
+var done = make(chan struct{}) // signal to stop traversal
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -22,6 +23,12 @@ func main() {
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
+
+	// Cancel traversal when input is detected.
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		close(done)
+	}()
 
 	// Traverse the file tree.
 	fileSizes := make(chan int64)
@@ -46,6 +53,12 @@ func main() {
 loop:
 	for {
 		select {
+		case <-done:
+			// Drain fileSizes to allow existing goroutines to finish
+			for range fileSizes {
+				// Do nothing.
+			}
+			return
 		case size, ok := <-fileSizes:
 			if !ok {
 				break loop
@@ -62,6 +75,10 @@ loop:
 
 func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 	defer n.Done()
+	if cancelled() {
+		return
+	}
+
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			n.Add(1)
@@ -74,7 +91,12 @@ func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 }
 
 func dirents(dir string) []os.FileInfo {
-	sema <- struct{}{}
+	select {
+	case sema <- struct{}{}: // acquire token
+	case <-done:
+		return nil // cancelled
+	}
+
 	defer func() { <-sema }()
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -86,4 +108,13 @@ func dirents(dir string) []os.FileInfo {
 
 func printDiskUsage(nfiles, nbytes int64) {
 	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
 }
