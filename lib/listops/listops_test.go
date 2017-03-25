1dbeba2fe37e750160aa323c5773463d4d334438
ll1615 <43992581@qq.com>
2017-03-25 14:08:41 +0800

    Create listops_test.go

---
+++ lib/listops/listops_test.go
@@ -0,0 +1,44 @@
+package listops
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestList(t *testing.T) {
+	Convey("test list operation ", t, func() {
+		list1 := []interface{}{
+			"python", "go", "shell", "java", "nodejs", "c#", "go", "go",
+		}
+		list2 := []interface{}{
+			"go", "lisp", "javascript", "html", "css",
+		}
+		list3 := []interface{}{}
+		list4 := []interface{}{
+			1, 2, 3, 4, 5, 6, 7,
+		}
+		list5 := []interface{}{
+			5, 6, 7, 8, 9, 0,
+		}
+		list6 := []interface{}{
+			5, 6, 7, "lisp", "html",
+		}
+
+		So(len(ListMinus(list1, list2)), ShouldEqual, 5)
+		So(len(ListAnd(list1, list2)), ShouldEqual, 1)
+		So(len(UniqueList(list1)), ShouldEqual, 6)
+
+		So(len(ListMinus(list1, list3)), ShouldEqual, 8)
+		So(len(ListAnd(list1, list3)), ShouldEqual, 0)
+
+		So(len(ListMinus(list4, list5)), ShouldEqual, 4)
+		So(len(ListAnd(list4, list5)), ShouldEqual, 3)
+
+		So(len(ListMinus(list2, list6)), ShouldEqual, 3)
+		So(len(ListAnd(list2, list6)), ShouldEqual, 2)
+
+		So(len(ListMinus(list5, list6)), ShouldEqual, 3)
+		So(len(ListAnd(list5, list6)), ShouldEqual, 3)
+	})
+}
